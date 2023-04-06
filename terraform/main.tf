da930ed14b657a626824f272767cdb311d51e7f4
Dominik Kopec <kopec.dominik@gmai.com>
2023-04-05 21:55:08 -0400

    fix: argument "protected_branches" is required

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,6 +82,7 @@ resource "github_repository_environment" "this" {
   environment = local.safe_env
   repository  = data.github_repository.this.name
   deployment_branch_policy {
+    protected_branches = false
     custom_branch_policies = true
   }
 
